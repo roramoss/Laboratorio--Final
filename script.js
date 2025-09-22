73d81122c67a10aeb3c642f6002b049dcdc7daa0
Noxi-PC <tu_correo@ejemplo.com>
2025-09-21 21:42:11 -0300

    modificacion css

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Array con las rutas de las imágenes que quieres en el carrusel
-    const images = ['src/carrusel5.webp', 'src/carrusel6.webp', 'src/carrusel7.webp', 'src/carrusel 4.webp']; // Asegúrate de tener estas imágenes
+   
+    const images = ['src/carrusel5.webp', 'src/carrusel6.webp', 'src/carrusel7.webp', 'src/carrusel 4.webp']; 
 
-    // Selecciona el contenedor del carrusel
+   
     const slideshowContainer = document.querySelector('.slideshow-container');
 
-    // Crea dinámicamente las imágenes y agrégalas al carrusel
+   
     images.forEach(imgSrc => {
         const img = document.createElement('img');
         img.src = imgSrc;
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
         slideshowContainer.appendChild(img);
     });
 
-    // Ahora, el código del carrusel se ejecuta después de que las imágenes se crearon
+   
     let slideIndex = 0;
     const slides = document.querySelectorAll('.slideshow-img');
 
@@ -25,13 +25,13 @@ document.addEventListener('DOMContentLoaded', () => {
             slideIndex = 1;
         }
         slides[slideIndex - 1].style.display = 'block';
-        setTimeout(showSlides, 3000); // Cambia de imagen cada 3 segundos
+        setTimeout(showSlides, 3000); 
     }
 
-    // Iniciar el carrusel una vez que las imágenes se hayan agregado
+    
     showSlides();
     
-    // Lógica para los botones del carrusel
+   
     document.querySelector('.next').addEventListener('click', () => {
         slideIndex++;
         if (slideIndex > slides.length) {
